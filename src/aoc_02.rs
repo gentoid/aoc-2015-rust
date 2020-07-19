2b7ce57c8a9f722480a6f26179beb564df142574
Viktor Lazarev <taurus101v@gmail.com>
2020-07-19 16:07:00 +0200

    Solve 02/01

--- src/aoc_02.rs
+++ src/aoc_02.rs
@@ -1,3 +1,25 @@
+use crate::read_input::read_lines;
+
+fn input() -> Vec<[u32; 3]> {
+    let mut dimensions = vec![];
+    for line in read_lines(2) {
+        let split: Vec<u32> = line.split("x").map(|dim| dim.parse::<u32>().unwrap()).take(3).collect();
+        dimensions.push([split[0], split[1], split[2]]);
+    }
+
+    dimensions
+}
+
+pub fn aoc_02_01() -> u32 {
+    let mut area = 0;
+
+    for dim in input() {
+        area += calculate_area(dim[0], dim[1], dim[2]);
+    }
+
+    area
+}
+
 fn calculate_area(x: u32, y: u32, z: u32) -> u32 {
     let (min_a, min_b) = find_minimal(x, y, z);
     2 * (x * y + y * z + x * z) + min_a * min_b
