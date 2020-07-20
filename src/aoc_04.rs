f64a5d6fb298be1db55a50fbfd2ba4932f07ad35
Viktor Lazarev <taurus101v@gmail.com>
2020-07-20 18:12:37 +0200

    Solve 04 / 02

--- src/aoc_04.rs
+++ src/aoc_04.rs
@@ -5,24 +5,28 @@ fn input() -> String {
 }
 
 pub fn aoc_04_01() -> u32 {
-    mine(&input())
+    mine(&input(), "00000")
 }
 
-fn mine(input: &str) -> u32 {
+pub fn aoc_04_02() -> u32 {
+    mine(&input(), "000000")
+}
+
+fn mine(input: &str, start: &str) -> u32 {
     let mut number = 0;
     
     loop {
-        if starts_with_5_zeros(input, number) {
+        if starts_with(input, number, start) {
             return number;
         }
         number += 1;
     }
 }
 
-fn starts_with_5_zeros(input: &str, number: u32) -> bool {
+fn starts_with(input: &str, number: u32, start: &str) -> bool {
     let digest = md5::compute(format!("{}{}", input, number));
     let stringified = format!("{:x}", digest);
-    stringified.starts_with("00000")
+    stringified.starts_with(start)
 }
 
 #[cfg(test)]
@@ -31,11 +35,11 @@ mod tests {
 
     #[test]
     fn mine_coinn_for_abcdef() {
-        assert_eq!(609043, mine("abcdef"));
+        assert_eq!(609043, mine("abcdef", "00000"));
     }
 
     #[test]
     fn mine_coinn_for_pqrstuv() {
-        assert_eq!(1048970, mine("pqrstuv"));
+        assert_eq!(1048970, mine("pqrstuv", "00000"));
     }
 }
