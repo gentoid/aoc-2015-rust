2318be7113700d8a47854b99b8bfd56e405ecc2e
Viktor Lazarev <taurus101v@gmail.com>
2020-07-21 20:50:03 +0200

    day 5: Compute updated niceness

--- src/aoc_05.rs
+++ src/aoc_05.rs
@@ -1,4 +1,5 @@
 use crate::read_input::read_lines;
+use std::collections::HashMap;
 
 pub fn aoc_05_01() -> u32 {
     let mut num_of_nice_lines = 0;
@@ -14,13 +15,13 @@ pub fn aoc_05_01() -> u32 {
 fn is_nice(input: &str) -> bool {
     let naugty_list = vec!["ab", "cd", "pq", "xy"];
     let vowels = vec!['a', 'e', 'i', 'o', 'u'];
-    
+
     for item in naugty_list {
         if input.contains(item) {
             return false;
         }
     }
-    
+
     let mut num_of_vowels = 0;
     let mut has_double_letter = false;
     let mut prev_char = '_';
@@ -38,6 +39,60 @@ fn is_nice(input: &str) -> bool {
     }
 
     num_of_vowels >= 3 && has_double_letter
+}
+
+fn is_really_nice(line: &str) -> bool {
+    let mut pairs: HashMap<(char, char), Vec<usize>> = HashMap::new();
+    let mut letters_cache: HashMap<char, usize> = HashMap::new();
+
+    let mut prev_char = None;
+    let mut contains_repeated_letter = false;
+
+    for (index, c) in line.char_indices() {
+        if !contains_repeated_letter {
+            match letters_cache.get_mut(&c) {
+                None => {
+                    letters_cache.insert(c, index);
+                }
+                Some(cached_index) => {
+                    if index - *cached_index == 2 {
+                        contains_repeated_letter = true;
+                    } else {
+                        *cached_index = index;
+                    }
+                }
+            }
+        }
+
+        if let Some(prev) = prev_char {
+            let pair = (prev, c);
+            match pairs.get_mut(&pair) {
+                None => {
+                    pairs.insert(pair, vec![index]);
+                }
+                Some(value) => value.push(index),
+            }
+        }
+        prev_char = Some(c);
+    }
+
+    let contains_pair_twice = pairs.iter().any(|(_, indices)| {
+        if indices.len() < 2 {
+            return false;
+        }
+        let min = indices.iter().min();
+        let max = indices.iter().max();
+
+        if let Some(max) = max {
+            if let Some(min) = min {
+                return max - min >= 2;
+            }
+        }
+
+        false
+    });
+
+    contains_pair_twice && contains_repeated_letter
 }
 
 #[cfg(test)]
@@ -67,5 +122,25 @@ mod tests {
     #[test]
     fn dvszwmarrgswjxmb_is_not_nice() {
         assert!(!is_nice("dvszwmarrgswjxmb"));
+    }
+
+    #[test]
+    fn qjhvhtzxzqqjkmpb_is_really_nice() {
+        assert!(is_really_nice("qjhvhtzxzqqjkmpb"));
+    }
+
+    #[test]
+    fn xxyxx_is_really_nice() {
+        assert!(is_really_nice("xxyxx"));
+    }
+
+    #[test]
+    fn uurcxstgmygtbstg_is_really_not_nice() {
+        assert!(!is_really_nice("uurcxstgmygtbstg"));
+    }
+
+    #[test]
+    fn ieodomkazucvgmuy_is_really_not_nice() {
+        assert!(!is_really_nice("ieodomkazucvgmuy"));
     }
 }
