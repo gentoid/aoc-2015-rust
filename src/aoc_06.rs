ea1a3ba3206c01fa883c50db8712632663ac1317
Viktor Lazarev <taurus101v@gmail.com>
2020-07-22 22:24:23 +0200

    day 6: add lines parses

--- src/aoc_06.rs
+++ src/aoc_06.rs
@@ -1,4 +1,47 @@
+use regex::Regex;
 use std::collections::HashMap;
+
+fn parse_line(line: &String) -> InstructionLine {
+    let mut instruction = Instruction::Toggle;
+    if line.starts_with("turn off") {
+        instruction = Instruction::TurnOff;
+    } else if line.starts_with("turn on") {
+        instruction = Instruction::TurnOn;
+    }
+
+    let digits =
+        Regex::new(r"([[:digit:]]+),([[:digit:]]+) through ([[:digit:]]+),([[:digit:]]+)$")
+            .unwrap();
+
+    let captures = digits.captures(line).unwrap();
+    println!("{:#?}", captures);
+
+    InstructionLine {
+        instruction,
+        from: (
+            captures[1].parse::<u32>().unwrap(),
+            captures[2].parse::<u32>().unwrap(),
+        ),
+        to: (
+            captures[3].parse::<u32>().unwrap(),
+            captures[4].parse::<u32>().unwrap(),
+        ),
+    }
+}
+
+#[derive(PartialEq, Debug)]
+enum Instruction {
+    TurnOn,
+    TurnOff,
+    Toggle,
+}
+
+#[derive(PartialEq, Debug)]
+struct InstructionLine {
+    instruction: Instruction,
+    from: (u32, u32),
+    to: (u32, u32),
+}
 
 struct LightsField {
     matrix: HashMap<(u32, u32), bool>,
@@ -7,7 +50,7 @@ struct LightsField {
 impl LightsField {
     fn new() -> Self {
         LightsField {
-            matrix: HashMap::new()
+            matrix: HashMap::new(),
         }
     }
 
@@ -39,7 +82,11 @@ impl LightsField {
     }
 
     fn count(&self) -> usize {
-        self.matrix.iter().filter(|(_, &value)| value).collect::<Vec<_>>().len()
+        self.matrix
+            .iter()
+            .filter(|(_, &value)| value)
+            .collect::<Vec<_>>()
+            .len()
     }
 }
 
@@ -57,5 +104,44 @@ mod tests {
         let mut lights = LightsField::new();
         lights.turn_on((1, 1), (4, 4));
         assert_eq!(lights.count(), 16);
+    }
+
+    #[test]
+    fn parses_turn_on_line() {
+        let line = "turn on 123,65 through 43,92".to_string();
+        assert_eq!(
+            parse_line(&line),
+            InstructionLine {
+                instruction: Instruction::TurnOn,
+                from: (123, 65),
+                to: (43, 92),
+            }
+        );
+    }
+
+    #[test]
+    fn parses_turn_off_line() {
+        let line = "turn off 23,675 through 56,962".to_string();
+        assert_eq!(
+            parse_line(&line),
+            InstructionLine {
+                instruction: Instruction::TurnOff,
+                from: (23, 675),
+                to: (56, 962),
+            }
+        );
+    }
+
+    #[test]
+    fn parses_toggle_line() {
+        let line = "toggle 37,63 through 678,45".to_string();
+        assert_eq!(
+            parse_line(&line),
+            InstructionLine {
+                instruction: Instruction::Toggle,
+                from: (37,63),
+                to: (678,45),
+            }
+        );
     }
 }
