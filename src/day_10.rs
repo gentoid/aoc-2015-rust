e3bc5160e48ad2bd3f66cb8df931a00a151a74b2
Viktor Lazarev <taurus101v@gmail.com>
2022-11-17 21:36:15 +0100

    Solve day 10, part 1

---
+++ src/day_10.rs
@@ -0,0 +1,56 @@
+const INPUT: &str = "1321131112";
+
+pub fn part_1() -> usize {
+    let mut data = INPUT.to_owned();
+
+    for _ in 1..=40 {
+        data = look_and_say(&data);
+    }
+
+    data.len()
+}
+
+fn look_and_say(input: &str) -> String {
+    let mut output = String::new();
+    
+    let mut chars = input.chars();
+
+    let mut prev_char = chars.next().expect("There must be a char");
+    let mut repetitions = 1;
+
+    for char in chars {
+        if char == prev_char {
+            repetitions += 1;
+        } else {
+            output.push_str(&format!("{repetitions}{prev_char}"));
+
+            prev_char = char;
+            repetitions = 1;
+        }
+    }
+    
+    output.push_str(&format!("{repetitions}{prev_char}"));
+
+    output
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn examples() {
+        let data = vec![
+            ("1", "11"),
+            ("11", "21"),
+            ("21", "1211"),
+            ("1211", "111221"),
+            ("111221", "312211"),
+        ];
+
+        for (input, ouput) in data {
+            assert_eq!(look_and_say(input), ouput.to_owned());
+        }
+    }
+}
