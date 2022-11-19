cef7171d9217d764a9718d7de54c9c5e27b14818
Viktor Lazarev <taurus101v@gmail.com>
2022-11-19 23:13:02 +0100

    Refactor code

--- src/day_13.rs
+++ src/day_13.rs
@@ -1,9 +1,13 @@
+use std::collections::HashMap;
+
 use regex::Regex;
 
-type Happiness = (String, String, i32);
+use crate::utils::without;
+
+type HappinessOptions = HashMap<(String, String), i32>;
 type Seating = (i32, String, i32);
 
-fn parse_line(input: &str) -> Happiness {
+fn parse_line(input: &str) -> (String, String, i32) {
     let temlate =
         Regex::new(r"(\w+) would (\w+) (\d+) happiness units by sitting next to (\w+)\.").unwrap();
     let captures = temlate.captures(input).unwrap();
@@ -21,56 +25,47 @@ fn parse_line(input: &str) -> Happiness {
     )
 }
 
-fn find_combinations(data: &Vec<Happiness>) -> Vec<Vec<Seating>> {
+fn find_combinations(options: &HappinessOptions, guests: &[String]) -> Vec<Vec<Seating>> {
     let mut output = vec![];
 
-    let first = data.first().unwrap();
-    let (filtered_first, filtered_second, filtered_out) = split_by_person(&first.0, data);
+    let first_guest = guests.first().unwrap();
+    let filtered = without(guests, first_guest);
 
-    if filtered_out.is_empty() {
-        let last = data.get(1).unwrap();
+    if filtered.len() == 1 {
         let mut tmp = vec![];
-        tmp.push((last.2, last.0.clone(), last.2));
-        tmp.push((first.2, first.0.clone(), first.2));
+
+        let last_guest = guests.last().unwrap();
+
+        let happiness = options
+            .get(&(first_guest.clone(), last_guest.clone()))
+            .unwrap();
+        let reverse_happiness = options
+            .get(&(last_guest.clone(), first_guest.clone()))
+            .unwrap();
+
+        tmp.push((*happiness, first_guest.clone(), *happiness));
+        tmp.push((*reverse_happiness, last_guest.clone(), *reverse_happiness));
         output.push(tmp);
 
         let mut tmp = vec![];
-        tmp.push((first.2, first.0.clone(), first.2));
-        tmp.push((last.2, last.0.clone(), last.2));
+        tmp.push((*reverse_happiness, last_guest.clone(), *reverse_happiness));
+        tmp.push((*happiness, first_guest.clone(), *happiness));
         output.push(tmp);
     } else {
-        for mut inner in find_combinations(&filtered_out) {
+        for mut inner in find_combinations(options, &filtered) {
+            let happiness = options
+                .get(&(first_guest.clone(), inner.last().unwrap().1.clone()))
+                .unwrap();
             let inner_first = inner.first().unwrap();
             let inner_last = inner.last().unwrap();
 
-            let mut tmp = vec![(first.2, first.0.clone(), first.2)];
+            let mut tmp = vec![(*happiness, first_guest.clone(), *happiness)];
             tmp.extend(inner);
             output.push(tmp);
         }
     }
 
     return output;
-}
-
-fn split_by_person(
-    name: &str,
-    list: &Vec<Happiness>,
-) -> (Vec<Happiness>, Vec<Happiness>, Vec<Happiness>) {
-    let mut filtered_first = vec![];
-    let mut filtered_second = vec![];
-    let mut filtered_out = vec![];
-
-    for item in list.iter().cloned() {
-        if item.0 == name {
-            filtered_first.push(item);
-        } else if item.1 == name {
-            filtered_second.push(item);
-        } else {
-            filtered_out.push(item);
-        }
-    }
-
-    (filtered_first, filtered_second, filtered_out)
 }
 
 #[cfg(test)]
@@ -102,45 +97,48 @@ mod tests {
         }
     }
 
+    fn options() -> HashMap<(String, String), i32> {
+        let mut options = HashMap::new();
+
+        options.insert(("Alice".to_owned(), "Bob".to_owned()), 57);
+        options.insert(("Alice".to_owned(), "Clark".to_owned()), 4);
+        options.insert(("Bob".to_owned(), "Alice".to_owned()), 10);
+        options.insert(("Bob".to_owned(), "Clark".to_owned()), -1);
+        options.insert(("Clark".to_owned(), "Alice".to_owned()), 6);
+        options.insert(("Clark".to_owned(), "Bob".to_owned()), 3);
+
+        options
+    }
+
+    fn guests(length: usize) -> Vec<String> {
+        vec!["Alice".to_owned(), "Bob".to_owned(), "Clark".to_owned()].into_iter().take(length).collect()
+    }
+
     #[test]
     fn finds_seating_for_two_persons() {
-        let data = vec![
-            ("Alice".to_owned(), "Bob".to_owned(), 57),
-            ("Bob".to_owned(), "Alice".to_owned(), 10),
-        ];
-
         let expected = vec![
-            vec![(10, "Bob".to_owned(), 10), (57, "Alice".to_owned(), 57)],
             vec![(57, "Alice".to_owned(), 57), (10, "Bob".to_owned(), 10)],
+            vec![(10, "Bob".to_owned(), 10), (57, "Alice".to_owned(), 57)],
         ];
 
-        assert_eq!(find_combinations(&data), expected);
+        assert_eq!(find_combinations(&options(), &guests(2)), expected);
     }
 
     #[test]
     fn finds_seating_for_three_persons() {
-        let data = vec![
-            ("Alice".to_owned(), "Bob".to_owned(), 57),
-            ("Alice".to_owned(), "Clark".to_owned(), 4),
-            ("Bob".to_owned(), "Alice".to_owned(), 10),
-            ("Bob".to_owned(), "Clark".to_owned(), -1),
-            ("Clark".to_owned(), "Alice".to_owned(), 6),
-            ("Clark".to_owned(), "Bob".to_owned(), 3),
-        ];
-
         let expected = vec![
-            vec![
-                (57, "Alice".to_owned(), 4),
-                (6, "Clark".to_owned(), 3),
-                (-1, "Bob".to_owned(), 10),
-            ],
             vec![
                 (4, "Alice".to_owned(), 57),
                 (10, "Bob".to_owned(), -1),
                 (3, "Clark".to_owned(), 6),
             ],
+            vec![
+                (57, "Alice".to_owned(), 4),
+                (6, "Clark".to_owned(), 3),
+                (-1, "Bob".to_owned(), 10),
+            ],
         ];
 
-        assert_eq!(find_combinations(&data), expected);
+        assert_eq!(find_combinations(&options(), &guests(3)), expected);
     }
 }
