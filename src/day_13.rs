af78a6bd2bbf2a103d902756718e5e2149907c35
Viktor Lazarev <taurus101v@gmail.com>
2022-11-19 15:44:37 +0100

    Find combinations for 2 persons

--- src/day_13.rs
+++ src/day_13.rs
@@ -21,6 +21,41 @@ fn parse_line(input: &str) -> Happiness {
     )
 }
 
+fn find_combinations(data: &Vec<Happiness>) -> Vec<Vec<Seating>> {
+    let mut output = vec![];
+
+    let first = data.first().unwrap();
+    let (filtered_first, filtered_second, filtered_out) = split_by_person(&first.0, data);
+
+    if filtered_out.is_empty() {
+        let second = data.get(1).unwrap();
+        let mut tmp = vec![];
+        tmp.push((second.2, second.0.clone(), second.2));
+        tmp.push((first.2, first.0.clone(), first.2));
+        output.push(tmp);
+    }
+
+    return output;
+}
+
+fn split_by_person(name: &str, list: &Vec<Happiness>) -> (Vec<Happiness>, Vec<Happiness>, Vec<Happiness>) {
+    let mut filtered_first = vec![];
+    let mut filtered_second = vec![];
+    let mut filtered_out = vec![];
+
+    for item in list.iter().cloned() {
+        if item.0 == name {
+            filtered_first.push(item);
+        } else if item.1 == name {
+            filtered_second.push(item);
+        } else {
+            filtered_out.push(item);
+        }
+    }
+
+    (filtered_first, filtered_second, filtered_out)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -48,5 +83,20 @@ mod tests {
                 (person1.to_owned(), person2.to_owned(), happiness)
             );
         }
+    }
+
+    #[test]
+    fn finds_seating_for_two_persons() {
+        let data = vec![
+            ("Alice".to_owned(), "Bob".to_owned(), 57),
+            ("Bob".to_owned(), "Alice".to_owned(), 10),
+        ];
+
+        let expected = vec![vec![
+            (10, "Bob".to_owned(), 10),
+            (57, "Alice".to_owned(), 57),
+        ]];
+
+        assert_eq!(find_combinations(&data), expected);
     }
 }
