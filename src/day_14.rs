2e07dd92aafde03fa42ad0af20cef5e529910df2
Viktor Lazarev <taurus101v@gmail.com>
2022-11-20 23:32:21 +0100

    Prepare function & structs for the second part of the day 14

--- src/day_14.rs
+++ src/day_14.rs
@@ -4,6 +4,53 @@ struct Reindeer {
     resting_time: u32,
 }
 
+#[derive(Debug)]
+enum ReindeerState {
+    HaveToFlyFor(u32),
+    HaveToRestfor(u32),
+}
+
+struct ReindeerData {
+    reindeer: Reindeer,
+    state: ReindeerState,
+    distance: u32,
+    points: u32,
+}
+
+impl ReindeerData {
+    fn new(reindeer: Reindeer) -> Self {
+        let state = ReindeerState::HaveToFlyFor(reindeer.flying_time);
+
+        Self {
+            reindeer,
+            state,
+            distance: 0,
+            points: 0,
+        }
+    }
+    fn tick(&mut self) {
+        use ReindeerState::*;
+
+        match self.state {
+            HaveToRestfor(ref mut time) => {
+                *time -= 1;
+
+                if *time == 0 {
+                    self.state = HaveToFlyFor(self.reindeer.flying_time);
+                }
+            }
+            HaveToFlyFor(ref mut time) => {
+                self.distance += self.reindeer.flying_speed;
+                *time -= 1;
+
+                if *time == 0 {
+                    self.state = HaveToRestfor(self.reindeer.resting_time);
+                }
+            }
+        }
+    }
+}
+
 pub fn part_1() -> u32 {
     let time = 2503;
     vec![comet_reindeer(), dancer_reindeer()]
@@ -11,6 +58,21 @@ pub fn part_1() -> u32 {
         .map(|reindeer| distance(reindeer, time))
         .max()
         .unwrap()
+}
+
+pub fn part_2() -> u32 {
+    let mut comet_data = ReindeerData::new(comet_reindeer());
+    let mut dancer_data = ReindeerData::new(dancer_reindeer());
+
+    for _ in 0..2503 {
+        tick_reindeers(&mut comet_data, &mut dancer_data);
+    }
+
+    vec![comet_data.points, dancer_data.points]
+        .iter()
+        .max()
+        .unwrap()
+        .clone()
 }
 
 fn comet_reindeer() -> Reindeer {
@@ -46,6 +108,20 @@ fn distance(reindeer: &Reindeer, time: u32) -> u32 {
     cicles * km_per_cicle + distance(reindeer, time - cicles * full_cicle)
 }
 
+fn tick_reindeers(comet: &mut ReindeerData, dancer: &mut ReindeerData) {
+    comet.tick();
+    dancer.tick();
+
+    if comet.distance > dancer.distance {
+        comet.points += 1;
+    } else if dancer.distance > comet.distance {
+        dancer.points += 1;
+    } else {
+        comet.points += 1;
+        dancer.points += 1;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -66,5 +142,31 @@ mod tests {
         assert_eq!(distance(&dancer, 1), 16);
         assert_eq!(distance(&dancer, 10), 160);
         assert_eq!(distance(&dancer, 1000), 1056);
+    }
+
+    #[test]
+    fn part_2_competition_140_seconds() {
+        let mut comet_data = ReindeerData::new(comet_reindeer());
+        let mut dancer_data = ReindeerData::new(dancer_reindeer());
+
+        for _ in 0..140 {
+            tick_reindeers(&mut comet_data, &mut dancer_data);
+        }
+
+        assert_eq!(dancer_data.points, 139);
+        assert_eq!(comet_data.points, 1);
+    }
+
+    #[test]
+    fn part_2_competition_1000_seconds() {
+        let mut comet_data = ReindeerData::new(comet_reindeer());
+        let mut dancer_data = ReindeerData::new(dancer_reindeer());
+
+        for _ in 0..1000 {
+            tick_reindeers(&mut comet_data, &mut dancer_data);
+        }
+
+        assert_eq!(dancer_data.points, 689);
+        assert_eq!(comet_data.points, 312);
     }
 }
